2f6d915e723b31ba4e9d23d031119528e7381ecb
maziokey <okeyluvsu2004@yahoo.com>
2022-03-02 16:39:20 +0100

    summernote integrated

--- config/settings.py
+++ config/settings.py
@@ -36,8 +36,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ['parishbits.com', 'www.parishbits.com', 'stkevinsjikwoyi.parishbits.com']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['parishbits.com', 'www.parishbits.com', 'stkevinsjikwoyi.parishbits.com']
+#ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -117,7 +117,7 @@ DATABASES = {
         'PORT': env("DATABASE_PORT"),
     }
 }
-"""
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -171,8 +171,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-"""
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = env("EMAIL_HOST")
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
@@ -180,7 +180,7 @@ EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = env("EMAIL_PORT")
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'info@parishbits.com'
-"""
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -188,8 +188,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ROOT_HOSTCONF = 'config.hosts'
 DEFAULT_HOST = 'www'
-PARENT_HOST = 'parishbits.local:8000'
-#PARENT_HOST = 'parishbits.com'
+#PARENT_HOST = 'parishbits.local:8000'
+PARENT_HOST = 'parishbits.com'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
